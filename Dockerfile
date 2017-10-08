26e60e0ea3a570f3dd6f974451b507c32aaac994
spawnia <benedikt.franke@zoho.com>
2017-10-08 15:52:42 +0200

    Include handling for php cgi and bolt-specific config

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM nginx:alpine
 LABEL maintainer="Benedikt Franke <benedikt.franke@zoho.com>"
 
 ENV WEB_ROOT=/usr/share/nginx/html
+ENV PHP_SERVER=php
+ENV PHP_PORT=9000
 
 COPY content /
 
--- content/start.sh
+++ content/start.sh
@@ -2,5 +2,7 @@
 
 # Replace config values with ENV variables either set in Dockerfile or docker-compose.yml
 sed -i -e "s~WEB_ROOT~${WEB_ROOT}~g" /etc/nginx/conf.d/default.conf
+sed -i -e "s~PHP_SERVER~${PHP_SERVER}~g" /etc/nginx/conf.d/default.conf
+sed -i -e "s~PHP_PORT~${PHP_PORT}~g" /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
