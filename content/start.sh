8ab74734af9961e00b4fd337d9d5587c24618393
spawnia <benedikt.franke@zoho.com>
2017-10-08 16:53:43 +0200

    Edit path to php-fpm conf

--- content/start.sh
+++ content/start.sh
@@ -3,7 +3,7 @@
 # Replace config values with ENV variables either set in Dockerfile or docker-compose.yml
 sed -i -e "s~WEB_ROOT~${WEB_ROOT}~g" /etc/nginx/conf.d/default.conf
 
-sed -i -e "s~PHP_SERVER~${PHP_SERVER}~g" /etc/nginx/global.php-fpm.conf
-sed -i -e "s~PHP_PORT~${PHP_PORT}~g" /etc/nginx/global.php-fpm.conf
+sed -i -e "s~PHP_SERVER~${PHP_SERVER}~g" /etc/nginx/global/php-fpm.conf
+sed -i -e "s~PHP_PORT~${PHP_PORT}~g" /etc/nginx/global/php-fpm.conf
 
 nginx -g "daemon off;"
